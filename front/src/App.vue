baa8fda866ba193d920b6003fe88daa99b35ce0e
ikeraguero <ikerpires407@gmail.com>
2023-01-19 11:51:39 -0300

    Add Footer

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <AppHeader />
   <router-view/>
+  <AppFooter />
 </template>
 
 <script>
 import AppHeader from '@/components/AppHeader.vue'
+import AppFooter from '@/components/AppFooter.vue'
 
 export default {
   components: {
-    AppHeader
+    AppHeader,
+    AppFooter
   }
 }
 </script>
