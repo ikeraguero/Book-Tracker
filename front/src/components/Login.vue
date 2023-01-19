2f0e29df9f5b7de73089e8793e2c076a9bb27857
ikeraguero <ikerpires407@gmail.com>
2023-01-19 18:25:49 -0300

    Remove Login

--- front/src/components/Login.vue
+++ front/src/components/Login.vue
@@ -1,43 +1,9 @@
 <template>
     <div>
-        <h2>Login</h2>
-        <form v-on:submit="login">
-            <input type="email" name="email" /><br>
-            <input type="password" name="password" /><br>
-            <input type="submit" value="login" />
-        </form>
     </div>
 </template>
 
 <script>
-    import router from "../router"
-    import axios from "axios"
-
-    export default {
-        name: "Login",
-        methods: {
-            login: (e) => {
-                e.preventDefault()
-                let email = "user@email.com"
-                let password = "password"
-                let login = () => {
-                    let data = {
-                        email: email,
-                        password: password
-                    }
-                    axios.post("api/login", data)
-                        .then((responde) => {
-                        console.log("Logged in")
-                        router.push("/home")
-                    })
-                    .catch((errors) => {
-                        console.log("Cannot login")
-                    })
-                }
-                login()
-            }
-        }
-    }
 </script>
 
 <style>
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
       <div class="user">
-          Logged as {{ user.name }}
+          Logged as 
       </div>
     <div class="currently">
       Currently Reading:
@@ -19,36 +19,6 @@
 </template>
 
 <script>
-  import axios from "axios"
-  import router from "../router"
-
-  export default {
-    name: "Login",
-    data() {
-      return {
-        user: {
-          name: "Jesse"
-        }
-      }
-    },
-    methods: {
-      getUserData: function() {
-        let self = this
-        axios.get("api/user")
-        .then((response) => {
-          console.log(response)
-          self.$set(this, "user", response.data.user)
-        })
-        .catch((errors) => {
-          console.log(errors)
-          router.push("/")
-        })
-      }
-    },
-    mounted() {
-      this.getUserData()
-    }
-  }
 </script>
 
 <style>
